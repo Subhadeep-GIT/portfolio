ba894e770e05aee1a00c0c8b6de863ec187206a5
Subhadeep Ghosh <subhadeep051@icloud.com>
2025-09-22 23:09:16 +0530

    Version1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React from "react";
-import "./styles.css";
-import Header from "./components/Header";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import Education from "./components/Education";
-import Achievements from "./components/Achievements";
-import Certifications from "./components/Certifications";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import { Button } from "react-bits";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <About />
-      <Skills />
-      <Experience />
-      <Education />
-      <Achievements />
-      <Certifications />
-      <Contact />
-      <Footer />
+    <div>
+      <h1>My Portfolio V2</h1>
+      <Button variant="primary">Click Me</Button>
     </div>
   );
 }
