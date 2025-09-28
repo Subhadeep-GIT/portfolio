970aa92ec5fc822fb2f93a5eb8cd043c1c2bc8ea
Subhadeep Ghosh <subhadeep051@icloud.com>
2025-09-28 20:07:39 +0530

    Vite

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -5,7 +5,7 @@ const HeroImage = () => {
   return (
     <div className='flex justify-center items-center'>
       <img 
-        src={hostImage}
+        src='https://github.com/Subhadeep-GIT/portfolio/blob/prototypeV1/src/assets/images/200345305009.jpg?raw=true' 
         alt='Hostinger Horizons' 
       />
     </div>
