ad94fb56fa94e865388fe144049c8127baa35b9f
Subhadeep Ghosh <subhadeep051@icloud.com>
2025-09-28 20:05:57 +0530

    Vite

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
             <img 
               className="relative z-10 w-full h-full rounded-full object-cover border-4 border-white/20"
               alt="Subhadeep Ghosh - Full Stack Developer"
-             src="https://images.unsplash.com/photo-1575383596664-30f4489f9786" />
+             src="https://github.com/Subhadeep-GIT/portfolio/blob/prototypeV1/src/assets/images/200345305009.jpg?raw=true" />
           </motion.div>
 
           {/* Main Heading */}
